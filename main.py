84e7a2706baf09df84241adac3ef19adcf4420f9
jabka <fisyukovich24@gmail.com>
2021-06-23 20:55:41 +0300

    logger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import serial
+from flask import app
+import serial, app_logger
 
 class SerialPortConnection():
     def __init__(self, params, port_name, baudrate, pause):
@@ -6,10 +7,17 @@ class SerialPortConnection():
         self.port_name = port_name
         self.baudrate = baudrate
         self.pause = pause
-        self.ser = serial.Serial(self.port_name, self.baudrate, timeout=self.pause)  
+        self.ser = serial.Serial(self.port_name, self.baudrate, timeout=self.pause) 
+        self.logger = app_logger.logger(__name__) 
 
     def sendMessage(self, message):
-        self.ser.write(message)
+        try:
+            self.ser.write(message)
+        except Exception as ex:
+            self.logger.error(str(ex))
 
     def readMessage(self):
-        line = self.ser.readline()
+        try:    
+            line = self.ser.readline()
+        except Exception as ex:
+            self.logger.error(str(ex))
--- routes.py
+++ routes.py
@@ -1,8 +1,10 @@
 from flask import request, json, render_template
 from app import app
 from main import SerialPortConnection
+import app_logger
 
 arduino = SerialPortConnection(5, '5', 5, 5)  #params, port, baudrate, pause 
+logger = app_logger.logger(__name__)
 
 @app.route("/home", methods=['POST'])
 def create_report():
@@ -10,4 +12,4 @@ def create_report():
         json_data = arduino.readMessage()
         return json_data
     except Exception as ex:
-        return {'status': 'error', 'message': str(ex)}
+        logger.error(str(ex))
