6fa070552724d58084ee61f076d342867399150a
sichiiii <76742792+sichiiii@users.noreply.github.com>
2021-07-31 23:50:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     from routes import *
     
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0')
     
